de315051fa61045aaea12ed3598f6722dbc3f3d4
Semitia <3272979503@qq.com>
2024-06-17 22:32:51 +0800

    clear big files, train.py

---
+++ scripts/model.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.model1 = Sequential(
+            Conv2d(1, 32, 5, padding=4),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+    
+    
+if __name__ == '__main__':
+    tudui = Tudui()
+    input = torch.ones((64, 1, 28, 28))
+    output = tudui(input)
+    print(output.shape)
